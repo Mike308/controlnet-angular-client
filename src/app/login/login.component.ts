3cdb05b5fcc90325162457071d9714b3d8b4c353
Mikołaj Werner <m.werner9205@gmail.com>
2019-04-17 18:52:34 +0200

    implemented login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  loginStatusSubscription: Subscription;
+  loginForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private authService: AuthService) {
   }
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+  onLogin() {
+    this.authService.loginUser(this.loginForm.value.userName, this.loginForm.value.password);
+    console.log('Login: ' + this.loginForm.value.userName);
+    console.log('Password:' + this.loginForm.value.password);
+  }
 }
