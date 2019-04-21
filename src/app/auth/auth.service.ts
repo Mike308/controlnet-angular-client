4d015b19c85ce309a80c0bf861f6d19bbc816d2f
Mikołaj Werner <m.werner9205@gmail.com>
2019-04-22 00:09:41 +0200

    added ifLoggedIn field

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,13 +2,13 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, Subject} from 'rxjs';
 import {UserModel} from './model/user.model';
-import {any} from 'codelyzer/util/function';
 
 @Injectable()
 export class AuthService {
 
   token: string;
   apiUrl = 'http://localhost:8080';
+  ifLoggedIn = false;
   loginStatus = new Subject<UserModel>();
 
   constructor(private httpClient: HttpClient) {
@@ -19,9 +19,10 @@ export class AuthService {
       (result) => {
         this.loginStatus.next(new UserModel(name, true, '  '));
         this.token = result.token;
+        this.ifLoggedIn = true;
         console.log('Token: ' + result.token);
       },
-        error1 => this.loginStatus.next(new UserModel(' ', false, ' '))
+      error1 => this.loginStatus.next(new UserModel(' ', false, ' '))
     );
   }
 
