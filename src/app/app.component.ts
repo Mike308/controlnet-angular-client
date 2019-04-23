97e7bfc6c1acd6c7a28135cd81b5e4a8031b6dab
Mikołaj Werner <m.werner9205@gmail.com>
2019-04-23 18:44:41 +0200

    added authorization handling to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from './auth/auth.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'controlnet-angular-client';
+  loginStatusSubscription: Subscription;
+  ifLoggedIn = false;
+
+  constructor (private authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    this.loginStatusSubscription = this.authService.loginStatus.subscribe(
+      (loginStatus) => {
+        this.ifLoggedIn = loginStatus.ifLoggedIn;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.loginStatusSubscription.unsubscribe();
+  }
 }
 
 
