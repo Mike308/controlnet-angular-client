3f7be4cb9a5a2938e12711c09f59c89db94e7d12
Mikołaj Werner <m.werner9205@gmail.com>
2019-04-25 21:37:47 +0200

    added getting sensors hub

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {DashboardService} from './service/dashboard.service';
+import {HubModel} from './model/hub.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,14 +10,22 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) { }
   title: string;
+  hubSensor: HubModel;
+
+  constructor(private activatedRoute: ActivatedRoute, private dashboardService: DashboardService) {
+  }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(
       (params) => {
         console.log('Module Id: ' + params.moduleId);
-        this.title = params.moduleId;
+        this.dashboardService.getSensorHub(params.moduleId).subscribe(
+          (result) => this.hubSensor = result,
+          error1 => {
+            console.log(error1);
+          }
+        );
       }
     );
   }
