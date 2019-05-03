f8e57471adbd2048e72fd406321c6220a9a47e65
Mikołaj Werner <m.werner9205@gmail.com>
2019-05-04 00:01:30 +0200

    added method for get humidity and light intensity measurements added necessarily methods for mapping data from api to data for chart

--- src/app/chart/service/chart.service.ts
+++ src/app/chart/service/chart.service.ts
@@ -5,6 +5,8 @@ import {Observable} from 'rxjs';
 import {TemperatureModel} from '../model/temperature.model';
 import {envDevs} from '../../shared/env.defs';
 import {ChartDataModel} from '../model/chart.data.model';
+import {HumidityMeasurementModel} from '../model/humidity.measurement.model';
+import {LightIntensityMeasurementModel} from '../model/light.intensity.measurement.model';
 
 @Injectable()
 export class ChartService {
@@ -21,10 +23,20 @@ export class ChartService {
 
   getTemperatures(moduleId: number, startDate: string, endDate: string): Observable<TemperatureModel[]> {
     return this.httpClient.get<TemperatureModel[]>(envDevs.url +
-      '/temperatures/module-id/1/start-date/2019-02-13 19:00/end-date/2019-02-13 20:01', this.httpOptions);
+      '/temperatures/module-id/' + moduleId + '/start-date/' + startDate + '/end-date/' + endDate, this.httpOptions);
   }
 
-  mapDataToChartData(temperatures: TemperatureModel[]): any {
+  getHumidityMeasurements(moduleId: number, startDate: string, endDate: string): Observable<HumidityMeasurementModel[]> {
+    return this.httpClient.get<HumidityMeasurementModel[]>(envDevs.url +
+      '/humidity/module-id/' + moduleId + '/start-date/' + startDate + '/end-date/' + endDate, this.httpOptions);
+  }
+
+  getLightIntensityMeasurements(moduleId: number, startDate: string, endDate: string): Observable<LightIntensityMeasurementModel[]> {
+    return this.httpClient.get<LightIntensityMeasurementModel[]>(envDevs.url +
+      '/light-intensity/module-id/' + moduleId + '/start-date/' + startDate + '/end-date/' + endDate, this.httpOptions);
+  }
+
+  mapTemperaturesToChartData(temperatures: TemperatureModel[]): any {
     const chartData: ChartDataModel[] = [];
     const slotNames = temperatures.map(value => {
       return {slotName: value.slotName, sensorId: value.sensorId};
@@ -39,6 +51,39 @@ export class ChartService {
     });
     return chartData;
   }
+
+  mapHumidityMeasurementsToChartData(humidityMeasurements: HumidityMeasurementModel[]): any {
+    const chartData: ChartDataModel[] = [];
+    const slotNames = humidityMeasurements.map(value => {
+      return {slotName: value.slotName, sensorId: value.sensorId};
+    }).filter((value, index, array) => {
+      return array.indexOf(array.find(t => t.sensorId === value.sensorId)) === index;
+    });
+    slotNames.forEach(sensor => {
+      const humidityMeasurementsOfSlot = humidityMeasurements.filter(x => x.sensorId === sensor.sensorId).map(value1 => {
+        return value1.humidity;
+      });
+      chartData.push(new ChartDataModel(sensor.slotName, humidityMeasurementsOfSlot));
+    });
+    return chartData;
+  }
+
+  mapLightIntensityMeasurementsToChartData(lightIntensityMeasurements: LightIntensityMeasurementModel[]): any {
+    const chartData: ChartDataModel[] = [];
+    const slotNames = lightIntensityMeasurements.map(value => {
+      return {slotName: value.slotName, sensorId: value.sensorId};
+    }).filter((value, index, array) => {
+      return array.indexOf(array.find(t => t.sensorId === value.sensorId)) === index;
+    });
+    slotNames.forEach(sensor => {
+      const lightIntensityMeasurementsOfSlot = lightIntensityMeasurements.filter(x => x.sensorId === sensor.sensorId).map(value1 => {
+        return value1.lightIntensity;
+      });
+      chartData.push(new ChartDataModel(sensor.slotName, lightIntensityMeasurementsOfSlot));
+    });
+    return chartData;
+  }
+
 
   getDateForXAxis(data: any[]): any {
     return  data.map(value => {
