576b2b55e1e800172c74d836fbbc86750f5cb3a5
Mikołaj Werner <m.werner9205@gmail.com>
2019-05-04 00:15:55 +0200

    code refactored

--- src/app/dashboard/model/hub.model.ts
+++ src/app/dashboard/model/hub.model.ts
@@ -1,12 +1,12 @@
 import {ModuleModel} from './module.model';
 import {TemperatureModel} from './temperature.model';
-import {HumidityMeasurementsModel} from './humidity.measurements.model';
+import {HumidityMeasurementModel} from './humidity.measurement.model';
 import {LightIntensityMeasurements} from './light.intensity.measurements';
 
 export class HubModel {
   modudle: ModuleModel;
   temperatures: TemperatureModel[] = [];
-  humidityMeasurements: HumidityMeasurementsModel[] = [];
+  humidityMeasurements: HumidityMeasurementModel[] = [];
   lightIntensityMeasurements: LightIntensityMeasurements[] = [];
 }
 
