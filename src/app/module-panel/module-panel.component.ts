f0ba9c32b810f005bcee5ada2fc6d30554c21168
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-24 17:54:28 +0100

    added module-panel component

---
+++ src/app/module-panel/module-panel.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-module-panel',
+  templateUrl: './module-panel.component.html',
+  styleUrls: ['./module-panel.component.css']
+})
+export class ModulePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onNewModuleClick() {
+
+  }
+
+  onEditModuleClick() {
+
+  }
+}
