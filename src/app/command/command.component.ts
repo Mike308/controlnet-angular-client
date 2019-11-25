9d5369f8d5d4d80f3bd390a28819490bae646062
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-25 21:42:23 +0100

    added and implemented command component (popup dialog)

---
+++ src/app/command/command.component.ts
@@ -0,0 +1,38 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {CommandModel} from '../shared/model/command.model';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-command',
+  templateUrl: './command.component.html',
+  styleUrls: ['./command.component.css']
+})
+export class CommandComponent implements OnInit {
+  commandFormGroup: FormGroup;
+  newCommand: CommandModel;
+
+
+  constructor(public dialogRef: MatDialogRef<CommandComponent>, @Inject(MAT_DIALOG_DATA) public data: CommandModel) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.commandFormGroup = new FormGroup({
+      commandCtrl: new FormControl('', Validators.required),
+      commandOrderCtrl: new FormControl('', Validators.required)
+    });
+  }
+
+  onAdd() {
+    this.data = {
+      id: null,
+      moduleId: this.data.moduleId,
+      command: this.commandFormGroup.get('commandCtrl').value,
+      commandOrder: this.commandFormGroup.get('commandOrderCtrl').value
+    };
+    this.dialogRef.close(this.data);
+  }
+}
