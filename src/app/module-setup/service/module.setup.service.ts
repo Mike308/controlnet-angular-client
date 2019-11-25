c170f7c711125f4dc10f6f4824ff0237c4bad8ce
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-25 21:41:21 +0100

    added methods for fetching commands and inserting new module

--- src/app/module-setup/service/module.setup.service.ts
+++ src/app/module-setup/service/module.setup.service.ts
@@ -4,6 +4,8 @@ import {Observable} from 'rxjs';
 import {DictionaryModel} from '../../shared/model/dictionary.model';
 import {environment} from '../../../environments/environment';
 import {AuthService} from '../../auth/auth.service';
+import {ModuleModel} from '../../shared/model/module.model';
+import {CommandModel} from '../../shared/model/command.model';
 
 @Injectable()
 export class ModuleSetupService {
@@ -17,9 +19,15 @@ export class ModuleSetupService {
     })
   };
 
-
   getModulesTypes(): Observable<DictionaryModel[]> {
     return this.httpClient.get<DictionaryModel[]>(environment.url + '/dictionary/modules-types', this.httpOptions);
   }
 
+  getCommands(moduleId: number): Observable<CommandModel[]> {
+    return this.httpClient.get<CommandModel[]>(environment.url + '/commands/' + moduleId);
+  }
+
+  addModule(module: ModuleModel): Observable<number> {
+    return this.httpClient.post<number>(environment.url + '/modules/add-new-module', module, this.httpOptions);
+  }
 }
