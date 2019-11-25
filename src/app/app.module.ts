1dbce66553ff2efe6a410dd2b76137226c46ab02
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-25 21:46:45 +0100

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {
   MatDialogModule,
   MatFormFieldModule,
   MatInputModule,
-  MatListModule, MatNativeDateModule
+  MatListModule, MatNativeDateModule, MatOptionModule, MatSelectModule, MatSnackBarModule, MatStepperModule, MatTableModule
 } from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -21,7 +21,7 @@ import { IndexComponent } from './index/index.component';
 import { NavComponent } from './nav/nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {NavService} from './nav/service/nav.service';
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutingModule} from './app.routing.module';
 import {AuthGuard} from './guard/auth.guard';
 import {DashboardService} from './dashboard/service/dashboard.service';
 import {GaugesModule} from 'ng-canvas-gauges';
@@ -32,6 +32,10 @@ import {ChartsModule} from 'ng2-charts';
 import { PopupDateDialogComponent } from './popup-date-dialog/popup-date-dialog.component';
 import {DatePipe} from '@angular/common';
 import { PopupSlotNameSetupComponent } from './popup-slot-name-setup/popup-slot-name-setup.component';
+import { CommandComponent } from './command/command.component';
+import { ModuleSetupComponent } from './module-setup/module-setup.component';
+import { ModulePanelComponent } from './module-panel/module-panel.component';
+import {ModuleSetupService} from './module-setup/service/module.setup.service';
 
 @NgModule({
   declarations: [
@@ -42,9 +46,12 @@ import { PopupSlotNameSetupComponent } from './popup-slot-name-setup/popup-slot-
     DashboardComponent,
     ChartComponent,
     PopupDateDialogComponent,
-    PopupSlotNameSetupComponent
+    PopupSlotNameSetupComponent,
+    CommandComponent,
+    ModuleSetupComponent,
+    ModulePanelComponent
   ],
-  entryComponents: [DashboardComponent, PopupDateDialogComponent, PopupSlotNameSetupComponent],
+  entryComponents: [DashboardComponent, PopupDateDialogComponent, PopupSlotNameSetupComponent, CommandComponent],
   imports: [
     BrowserModule,
     MatButtonModule,
@@ -64,9 +71,14 @@ import { PopupSlotNameSetupComponent } from './popup-slot-name-setup/popup-slot-
     MatDialogModule,
     ChartsModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatTableModule
   ],
-  providers: [AuthService, NavService, AuthGuard, DashboardService, ChartService, DatePipe],
+  providers: [AuthService, NavService, AuthGuard, DashboardService, ChartService, ModuleSetupService, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
