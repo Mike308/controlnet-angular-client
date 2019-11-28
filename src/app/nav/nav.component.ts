3435d7b5b55209d5eaab87ab1bdcc0fdf540cd74
Mikołaj Werner <m.werner9205@gmail.com>
2019-11-28 18:31:20 +0100

    replaced nav service on module service and added some improvement (integration with module setup component)

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ModuleModel} from './model/module.model';
 import {NavService} from './service/nav.service';
+import {ModuleService} from '../shared/service/module.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
 
   modules: ModuleModel[] = [];
+  newModuleSubscription: Subscription;
 
-  constructor(private navService: NavService) { }
+  constructor(private moduleService: ModuleService) { }
 
   ngOnInit() {
-    this.navService.getModules().subscribe(
+
+    this.getModules();
+    this.newModuleSubscription  = this.moduleService.newModuleInserted.subscribe(moduleId => {
+      this.getModules();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.newModuleSubscription.unsubscribe();
+  }
+
+  getModules() {
+    this.moduleService.getModules().subscribe(
       (modules => this.modules = modules),
       error1 => console.log('Error: ' + error1)
     );
