fcb83b6585173e3db82439e2e66251150f5a4333
Mikołaj Werner <m.werner9205@gmail.com>
2019-12-28 00:13:06 +0100

    changed source of http options

--- src/app/shared/service/module.service.ts
+++ src/app/shared/service/module.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable, Subject} from 'rxjs';
 import {ModuleModel} from '../model/module.model';
 import {envDevs} from '../env.defs';
@@ -12,50 +12,44 @@ import {CommandModel} from '../model/command.model';
 export class ModuleService {
 
   newModuleInserted = new Subject<number>();
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-type': 'application/json',
-      Authorization: this.authService.token
-    })
-  };
 
   constructor(private httpClient: HttpClient, private authService: AuthService) {
   }
 
   getModules(): Observable<ModuleModel[]> {
-    return this.httpClient.get<ModuleModel[]>(envDevs.url + '/modules/all', this.httpOptions);
+    return this.httpClient.get<ModuleModel[]>(envDevs.url + '/modules/all', this.authService.httpOptions);
   }
 
   getModule(moduleId: number): Observable<ModuleModel> {
-    return this.httpClient.get<ModuleModel>(envDevs.url + '/modules/module/' + moduleId, this.httpOptions);
+    return this.httpClient.get<ModuleModel>(envDevs.url + '/modules/module/' + moduleId, this.authService.httpOptions);
   }
 
   deleteModule(moduleId: number): Observable<void> {
-    return this.httpClient.get<void>(envDevs.url + '/modules/delete-module/' + moduleId, this.httpOptions);
+    return this.httpClient.get<void>(envDevs.url + '/modules/delete-module/' + moduleId, this.authService.httpOptions);
   }
 
   getModulesTypes(): Observable<DictionaryModel[]> {
-    return this.httpClient.get<DictionaryModel[]>(environment.url + '/dictionary/modules-types', this.httpOptions);
+    return this.httpClient.get<DictionaryModel[]>(environment.url + '/dictionary/modules-types', this.authService.httpOptions);
   }
 
   getCommands(moduleId: number): Observable<CommandModel[]> {
-    return this.httpClient.get<CommandModel[]>(environment.url + '/commands/' + moduleId, this.httpOptions);
+    return this.httpClient.get<CommandModel[]>(environment.url + '/commands/' + moduleId, this.authService.httpOptions);
   }
 
   addModule(module: ModuleModel): Observable<number> {
-    return this.httpClient.post<number>(environment.url + '/modules/add-new-module', module, this.httpOptions);
+    return this.httpClient.post<number>(environment.url + '/modules/add-new-module', module, this.authService.httpOptions);
   }
 
   addCommands(commands: CommandModel): Observable<CommandModel> {
-    return this.httpClient.post<CommandModel>(environment.url + '/commands/add', commands, this.httpOptions);
+    return this.httpClient.post<CommandModel>(environment.url + '/commands/add', commands, this.authService.httpOptions);
   }
 
   deleteCommand(id: number): Observable<void> {
-    return this.httpClient.get<void>(environment.url + '/commands/delete/' + id, this.httpOptions);
+    return this.httpClient.get<void>(environment.url + '/commands/delete/' + id, this.authService.httpOptions);
   }
 
   deleteCommands(moduleId: number): Observable<void> {
-    return this.httpClient.get<void>(environment.url + '/commands/delete-all/' + moduleId, this.httpOptions);
+    return this.httpClient.get<void>(environment.url + '/commands/delete-all/' + moduleId, this.authService.httpOptions);
   }
 
 
