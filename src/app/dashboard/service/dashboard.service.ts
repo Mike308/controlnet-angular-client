74abc2b2d5954c6b998358b715bf3169a42d0640
Mikołaj Werner <m.werner9205@gmail.com>
2019-12-28 00:15:15 +0100

    replaced envDefs on environment

--- src/app/dashboard/service/dashboard.service.ts
+++ src/app/dashboard/service/dashboard.service.ts
@@ -1,24 +1,24 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {AuthService} from '../../auth/auth.service';
 import {Observable} from 'rxjs';
 import {HubModel} from '../model/hub.model';
-import {envDevs} from '../../shared/env.defs';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class DashboardService {
   constructor (private  httpClient: HttpClient, private authService: AuthService) { }
 
   getSensorHub(moduleId: number): Observable<HubModel> {
-    return this.httpClient.get<HubModel>(envDevs.url + '/hub/module/' + moduleId, this.authService.httpOptions);
+    return this.httpClient.get<HubModel>(environment.url + '/hub/module/' + moduleId, this.authService.httpOptions);
   }
 
   setSensorSlotName(sensorId: number, slotName: string): Observable<any> {
-    return this.httpClient.post<any>(envDevs.url + '/sensor/set-slot-name', {sensorId, newName: slotName}, this.authService.httpOptions);
+    return this.httpClient.post<any>(environment.url + '/sensor/set-slot-name', {sensorId, newName: slotName}, this.authService.httpOptions);
   }
 
   deleteAllMeasurmentsAndSensors(moduleId: number): Observable<void> {
-    return this.httpClient.get<void>(envDevs.url + '/hub/remove-sensors/' + moduleId, this.authService.httpOptions);
+    return this.httpClient.get<void>(environment.url + '/hub/remove-sensors/' + moduleId, this.authService.httpOptions);
   }
 
 }
