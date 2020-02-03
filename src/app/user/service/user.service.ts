b89271b4a567e155774f82916789a150991ada81
Mikołaj Werner <m.werner9205@gmail.com>
2020-02-03 21:33:58 +0100

    added user service

---
+++ src/app/user/service/user.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {AuthService} from '../../auth/auth.service';
+
+@Injectable()
+export class UserService {
+
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+  }
+
+  insertNewUser(user: { name: string, password: string }): Observable<any> {
+    return this.httpClient.post<any>(environment.url + '/user/insert', user, this.authService.httpOptions);
+  }
+}
