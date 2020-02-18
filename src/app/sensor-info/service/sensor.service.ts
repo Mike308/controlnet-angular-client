6a687614efd6f6b68ed7cd5898cca11b9ec91945
Mikołaj Werner <m.werner9205@gmail.com>
2020-02-18 18:10:36 +0100

    added http options

--- src/app/sensor-info/service/sensor.service.ts
+++ src/app/sensor-info/service/sensor.service.ts
@@ -3,14 +3,15 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {SensorModel} from '../model/sensor.model';
 import {environment} from '../../../environments/environment';
+import {AuthService} from '../../auth/auth.service';
 
 @Injectable()
 export class SensorService {
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
   }
 
   getSensorInfo(sensorId: number): Observable<SensorModel> {
-    return this.httpClient.get<SensorModel>(environment.url + '/sensor/info/' + sensorId);
+    return this.httpClient.get<SensorModel>(environment.url + '/sensor/info/' + sensorId, this.authService.httpOptions);
   }
 
 }
